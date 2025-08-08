784cdcb03424fce27cdb05f13c55cbe5f32aa0eb
Iqra Arif <bintearif79@gmail.com>
2025-08-08 18:51:53 +0500

    Add files via upload

---
+++ List practice.py
@@ -0,0 +1,7 @@
+#  syntax : my_list = [item1,item2,item3]
+students = ["Noor","Ayesha","Hira"]
+
+print("First student:",students[0])
+# for adding
+students.append("Laraib")
+print("students list:",students)
